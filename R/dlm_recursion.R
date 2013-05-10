b2e20588cb1dbaddf460e4430b191bce866abba4
Jeff Arnold <jeffrey.arnold@gmail.com>
2013-05-10 18:30:22 -0400

    fixes for time-varying parameters

--- R/dlm_recursion.R
+++ R/dlm_recursion.R
@@ -9,7 +9,8 @@ NULL
 #' \code{\linkS4class{"DLM"}} object.
 #'
 #' @param object \code{DLM}
-#' @param n \code{numeric} Number of samples to draw
+#' @param n \code{numeric} Number of samples to draw. If NULL, then
+#' \code{object} must have time-varying parameters.
 #' @param a1 \code{numeric} Initial state. If missing, a random
 #' initial state is drawn from the distribution of the initial state
 #' in \code{object}.
@@ -19,9 +20,20 @@ NULL
 #' \item{\code{y}}{\code{matrix} object with dimensions N x n.
 #' The first column is set to 0.}
 #' }
-dlm_recursion <- function(object, n, a1 = NULL) {
+dlm_recursion <- function(object, n = NULL, a1 = NULL) {
   m <- length(object@a1)
   N <- nrow(object@Z)
+
+  # Use whether n is specified to determine if it is time varying
+  if (is.null(n)) {
+    istv <- TRUE
+    n <- nrow(object@X)
+    if (n == 0) {
+      stop("if n = NULL, then object must have time-varying parameters")
+    }
+  } else {
+    istv <- FALSE
+  }
 
   # Save
   a <- matrix(0, m, n + 1)
@@ -37,12 +49,18 @@ dlm_recursion <- function(object, n, a1 = NULL) {
   }
   ## TODO:
   ## Speed ups
-  ## - draw all normal RV and multiply by cholesky of Omega
   ## - check if TV, and only draw delta / Phi / Omega if TV
+  if (! istv) {
+    delta <- dlm_delta(object)
+    Phi <- dlm_phi(object)
+    Omega <- dlm_Omega(object)
+  }
   for (i in 1:n) {
-    delta <- dlm_delta(object, i)
-    Phi <- dlm_phi(object, i)
-    Omega <- dlm_Omega(object, i)
+    if (istv) {
+      delta <- dlm_delta(object, i)
+      Phi <- dlm_phi(object, i)
+      Omega <- dlm_Omega(object, i)
+    }
     ay <- as.numeric(rmvnorm(1, delta + Phi %*% a[ , i], Omega))
     a[ , i + 1] <- ay[1:m]
     y[ , i] <- ay[m + 1:N]
